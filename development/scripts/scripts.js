633df0d94ba777e261f2d519182eb72cf6af01b4
Jay Bernal <jjbernal@gmail.com>
2016-06-30 16:23:50 -0500

    added jquery stuff

--- development/scripts/scripts.js
+++ development/scripts/scripts.js
@@ -1,21 +1,40 @@
 
 
-$(function() { // wait for document ready
-    // build scene
+$(function() {
+  var topoffset = 43;
   var controller = new ScrollMagic.Controller({
     globalSceneOptions: {
-      tiggerHook: "onLeave"
+      triggerHook: "onLeave"
     }
   });
+
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top - topoffset
+        }, 1000);
+        return false;
+      }
+    }
+  });
+
+  $(window).scroll(function(){
+    var windowpos = $(window).scrollTop + topoffset;
+    $("nav li a").removeClass("active");
+
+    if (windowpos > $("about-me").offset().top) {
+      $("nav la a").removeClass("active");
+      $('a[href$="about-me"]').addClass("active");
+    }
+  });
+
 
   var scene = new ScrollMagic.Scene({triggerElement: "#nav"})
     .setPin("#nav")
     .addTo(controller);
-
-
-
-
-
 
   });
 
